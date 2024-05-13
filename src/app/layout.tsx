cbf52d92c2d66a927d3e1e04f95c1142ead31640
Yu.Sun <Yu.Sun@xjtlu.edu.cn>
2024-05-13 16:56:38 +0800

    文件格式修改

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/app/component/Header";
+import Header from "@/component/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
