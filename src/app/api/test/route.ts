5a5ecc677ca23f49e127f2e65a427103ba994e55
fu-wa-fu <juanfufufu@sina.com>
2024-07-07 12:31:20 +0800

    测试代码

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,12 +4,11 @@ import { addTest } from "@/server/front/action/test";
 export async function POST(request) {
     const params = await request.json();
 
-    const result = await addTest(params);
-
+    console.log(params);
     return NextResponse.json({
         code: 0,
         message: "success",
         success: true,
-        data: result,
+        data: params,
     }, { status: 200 })
 } 
