c76d69ab0d4e9d6ab6f2c3f2348b7e9b5bc7a5a8
fu-wa-fu <juanfufufu@sina.com>
2024-07-08 00:58:35 +0800

    引入banner组件

--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 // sections
 import { Hero, SponsoredBy, AboutEvent, OurStats, EventContent, Faq } from "./(components)";
-
+import { Banner } from "@/components";
 export default function Page() {
 
   return (
     <>
+      <Banner />
       <Hero />
       <SponsoredBy />
       <AboutEvent />
