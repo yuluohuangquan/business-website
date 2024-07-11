61ade4ff93c3193e3c37b834b781cca26169bb14
fu-wa-fu <juanfufufu@sina.com>
2024-07-11 23:27:41 +0800

    部署报错修改

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,6 +3,6 @@ export function Title({title, url}) {
 
     return <div className="flex justify-between">
         <p className="font-bold">{title}</p>
-        <Link href={url} className="text-blue-300 hover:text-blue-500">更多>></Link>
+        <Link href={url} className="text-blue-300 hover:text-blue-500">更多&gt;&gt;</Link>
     </div>
 }
