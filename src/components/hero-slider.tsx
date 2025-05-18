9f1121edf2075014abec17f3079d3b8d5b94fc1e
unknown <648779925@qq.com>
2025-05-18 16:08:47 +0800

    图片修改

--- src/components/hero-slider.tsx
+++ src/components/hero-slider.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 // 导入 Swiper 样式
 import "swiper/css";
@@ -20,18 +21,59 @@ export function HeroSlider() {
   const slides: SlideType[] = [
     {
       id: 1,
-      image: "/images/home/banner1.png",
-      link: "/about"
+      image: "/images/home/banner1.jpg",
+      link: "/"
     },
     {
       id: 2,
-      image: "/images/home/banner2.png",
-      link: "/product"
+      image: "/images/home/banner2.jpg",
+      link: "/"
+    },
+    {
+      id: 3,
+      image: "/images/home/banner3.png",
+      link: "/"
+    },
+    {
+      id: 4,
+      image: "/images/home/banner4.jpg",
+      link: "/"
     }
   ];
 
+  // 根据设备宽度设置轮播图高度
+  const [bannerHeight, setBannerHeight] = useState("500px");
+
+  useEffect(() => {
+    // 设置初始高度
+    updateBannerHeight();
+    
+    // 监听窗口大小变化
+    window.addEventListener("resize", updateBannerHeight);
+    
+    // 清理监听器
+    return () => {
+      window.removeEventListener("resize", updateBannerHeight);
+    };
+  }, []);
+
+  // 更新高度函数
+  const updateBannerHeight = () => {
+    const width = window.innerWidth;
+    if (width < 640) {
+      // 移动设备
+      setBannerHeight("300px");
+    } else if (width < 1024) {
+      // 平板设备
+      setBannerHeight("400px");
+    } else {
+      // 桌面设备
+      setBannerHeight("500px");
+    }
+  };
+
   return (
-    <div className="relative w-full h-[500px]">
+    <div className="relative w-full" style={{ height: bannerHeight }}>
       <Swiper
         modules={[Autoplay, Navigation, Pagination]}
         spaceBetween={0}
@@ -45,28 +87,32 @@ export function HeroSlider() {
         className="w-full h-full"
       >
         {slides.map((slide) => (
-          <SwiperSlide key={slide.id} className="relative">
-            <div className="relative w-full h-full">
-              {slide.link ? (
-                <Link href={slide.link} className="block w-full h-full">
+          <SwiperSlide key={slide.id} className="relative h-full flex items-center justify-center">
+            {slide.link ? (
+              <Link href={slide.link} className="block w-full h-full flex items-center justify-center">
+                <div className="relative w-full h-full">
                   <Image
                     src={slide.image}
                     alt={`Banner ${slide.id}`}
                     fill
                     priority
-                    className="object-cover object-center"
+                    sizes="100vw"
+                    className="object-contain object-center"
                   />
-                </Link>
-              ) : (
+                </div>
+              </Link>
+            ) : (
+              <div className="relative w-full h-full">
                 <Image
                   src={slide.image}
                   alt={`Banner ${slide.id}`}
                   fill
                   priority
-                  className="object-cover object-center"
+                  sizes="100vw"
+                  className="object-contain object-center"
                 />
-              )}
-            </div>
+              </div>
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
